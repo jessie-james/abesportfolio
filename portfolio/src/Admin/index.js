f6d61903210b6459d8b0e3a8d93b432ba825acfe
Jessie James <jess.jamestwoo11@gmail.com>
2019-11-15 11:17:41 -0700

    package installs  check that the DB is running. get frame work on front end side or the admin running.

---
+++ portfolio/src/Admin/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './admin'
+
+function Admin() {
+    return (
+        <div>
+            hi this should be protected.
+         </div>
+     )
+}
+export default Admin;
--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from "./Home"
+import Admin from './Admin'
 import { Switch, Route } from 'react-router-dom';
 
 
@@ -8,6 +9,7 @@ function App() {
     <div className="app-container">
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route path='/Admin' component={Admin} />
         {/* <Route path='/NewYork' component={NewYork} />
         <Route path='/Summer' component={Summer} />
         <Route path='/Telos' component={Telos} /> */}
