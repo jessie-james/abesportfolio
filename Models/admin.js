44d9933de3ebedacdb9ec56c14a67713ff2686c5
Jessie James <jess.jamestwoo11@gmail.com>
2019-11-15 15:42:36 -0700

    install node modules

--- Models/admin.js
+++ Models/admin.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require("bcrypt");
+const bcrypt = require("./node_modules/bcrypt");
 
 const adminSchema = new Schema({
     username: {
--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -14,7 +14,6 @@ function App() {
         <Route path='/Summer' component={Summer} />
         <Route path='/Telos' component={Telos} /> */}
       </Switch>
-      
     </div>
   );
 }
